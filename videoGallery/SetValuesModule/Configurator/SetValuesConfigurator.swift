bdb9680ff1103b916ea81daf18d137f13092b1e2
CyrilusK <kcyrg17@gmail.com>
2024-10-02 20:04:08 +0300

    add customizationModule

---
+++ videoGallery/SetValuesModule/Configurator/SetValuesConfigurator.swift
@@ -0,0 +1,8 @@
+//
+//  SetValuesConfigurator.swift
+//  videoGallery
+//
+//  Created by Cyril Kardash on 02.10.2024.
+//
+
+import Foundation
---
+++ videoGallery/SetValuesModule/Presenter/SetValuesOutputProcotol.swift
@@ -0,0 +1,8 @@
+//
+//  SetValuesOutputProcotol.swift
+//  videoGallery
+//
+//  Created by Cyril Kardash on 02.10.2024.
+//
+
+import Foundation
